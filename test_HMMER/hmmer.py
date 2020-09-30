c8fc385431aaeb6548ffe379101b8633b62dc56c
LianaGrieken <lianavangrieken@gmail.com>
2020-09-30 09:15:44 +0200

    Implemented downloading hmm.dat.gz file

--- test_HMMER/hmmer.py
+++ test_HMMER/hmmer.py
@@ -13,23 +13,26 @@ sys.path.insert(0, "/mnt/d/Uni/Thesis_MultiGeneBlast/multigeneblast2.0")
 from utilities import *
 
 
-def check_pfam_db(path):
+def check_pfam_db(path, file_names):
     """Check f Pfam-A db exists else download
 
     :param path: String, path where to check
     """
-    url = "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam33.1/Pfam-A.hmm.gz"
-    if os.path.exists(path):
+    url_ls = ["ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam33.1/Pfam-A.hmm.gz",
+              "ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam33.1/Pfam-A.hmm.dat.gz"]
+    if os.path.exists(path + "Pfam-A.hmm.gz"):
         print("Pfam database found")
     else:
         print("Fetching database from Pfam release: 33.1 ")
-        try:
-            urllib.request.urlretrieve(url, path)
-            print("Saved:", path)
-        except FileNotFoundError:
-            print("Error: Path or file does not exists")
-        except urllib.error.URLError or urllib.error.HTTPError:
-            print("Error: Internet connection problem")
+        counter = 0
+        for url in url_ls:
+            try:
+                urllib.request.urlretrieve(url, path + file_names[counter])
+            except FileNotFoundError:
+                print("Error: Path or file does not exists")
+            except urllib.error.URLError or urllib.error.HTTPError:
+                print("Error: Internet connection problem")
+            counter += 1
 
 
 def fetch_profiles(keys, db):
@@ -40,11 +43,7 @@ def fetch_profiles(keys, db):
     """
     print("Fetching profiles from Pfam-A file")
     command_fetch_profile = "hmmfetch -o key.hmm -f {} {}".format(db, keys)
-    try:
-        run_commandline_command(command_fetch_profile, max_retries=1)
-    except MultiGeneBlastException:
-        raise MultiGeneBlastException("Key not found in file")
-    print("done fetching profiles")
+    subprocess.run(command_fetch_profile, shell=True)
 
 
 def run_hmmsearch(hmm_prof, db):
@@ -61,23 +60,24 @@ def run_hmmsearch(hmm_prof, db):
 
 def main():
     # this is for testing, change when implementing in main code #
-    pfam_db = "/mnt/d/Uni/Thesis_MultiGeneBlast/Pfam-A.hmm.gz"
+    pfam_db = "/mnt/d/Uni/Thesis_MultiGeneBlast/"
+    names_file = ["Pfam-A.hmm.gz", "Pfam-A.hmm.dat.gz"]
 
     # step 1: check if pfam db is present and if not download it
-    check_pfam_db(pfam_db)
+    check_pfam_db(pfam_db, names_file)
 
     # Step 2: Run hmmsearch
-    # 2a: Run with pfam accession name(s): use hmmfetch to get information from
+    # 2a: Run with pfam accession name(s): use hmmfetch to get profiles from
     # Pfam-A db.
     key_file = "/mnt/d/Uni/Thesis_MultiGeneBlast/key_file.txt"
-    #fetch_profiles(key_file, pfam_db)
+    fetch_profiles(key_file, pfam_db)
 
     # 2b: Run hmmsearch
     path_key = "/mnt/d/Uni/Thesis_MultiGeneBlast/key.hmm"
     path_db = "/mnt/d/Uni/Thesis_MultiGeneBlast/UP000008308_263358.fasta.gz"
-    run_hmmsearch(path_key, path_db)
+    #run_hmmsearch(path_key, path_db)
 
-    # step 3: parse results with biopython
+    # step 3: parse results with biopython package
 
 
 if __name__ == '__main__':
