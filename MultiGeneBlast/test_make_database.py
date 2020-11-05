02ce562c9f657d91a458c02e025c31064baf391c
Bram <eckelradekip10@gmail.com>
2020-11-05 14:07:39 +0100

    moved the test files to please linux

--- MultiGeneBlast/test_make_database.py
+++ MultiGeneBlast/test_make_database.py
@@ -23,7 +23,7 @@ def test_protein_database(command):
 
     # make sure that even when chaning directories the directory can be found
     my_path = os.path.abspath(os.path.dirname(__file__))
-    db_folder = os.path.join(my_path, "test_data_base")
+    db_folder = os.path.join(my_path, "tests{}test_data_base".format(os.sep))
 
     # check if all files are present
     db_folder_list = os.listdir(db_folder)
@@ -40,7 +40,7 @@ def test_nucleotide_database(command):
 
     # make sure that even when chaning directories the directory can be found
     my_path = os.path.abspath(os.path.dirname(__file__))
-    db_folder = os.path.join(my_path, "test_data_base")
+    db_folder = os.path.join(my_path, "tests{}test_data_base".format(os.sep))
 
     # check if all files are present
     db_folder_list = os.listdir(db_folder)
@@ -55,41 +55,41 @@ def test_nucleotide_database(command):
 if __name__ == "__main__":
     # basic command using gbk
     print("Starting Makedatabase with gbk file:")
-    command1 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1_ASM20415v1" \
-               "_genomic.gbk -inf all".format(os.sep, os.sep, os.sep)
+    command1 = "make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1_ASM20415v1" \
+               "_genomic.gbk -inf all".format(os.sep, os.sep)
     test_protein_database(command1)
     print("Finished running gbk test...")
     print()
 
     # command for embl
     print("Starting Makedatabase with embl file:")
-    command2 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1_ASM20415v1" \
-               "_genomic.embl -inf all".format(os.sep, os.sep, os.sep)
+    command2 = "make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1_ASM20415v1" \
+               "_genomic.embl -inf all".format(os.sep, os.sep)
     test_protein_database(command2)
     print("Finished running embl test...")
     print()
 
     # command for combined
     print("Starting Makedatabase with gbk and embl file file:")
-    command3 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1" \
+    command3 = "make_database.py -o tests{}test_data_base -n test -i tests{}GCA_000204155.1" \
                "_ASM20415v1_genomic.gbk tests{}GCA_000204155.1_ASM20415v1_genomic.embl -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_protein_database(command3)
     print("Finished running combined test...")
     print()
 
     # command for testing wgs master record
     print("Starting Makedatabase with WGS record file:")
-    command4 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}WGS_master_record_test.gb -inf all"\
-        .format(os.sep, os.sep, os.sep)
+    command4 = "make_database.py -o tests{}test_data_base -n test -i tests{}WGS_master_record_test.gb -inf all"\
+        .format(os.sep, os.sep)
     test_protein_database(command4)
     print("Finished running WGS test...")
     print()
 
     # command for testing rare cases (missing sequences incomplete entries etc.)
     print("Starting Makedatabase with rare cases file:")
-    command5 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}rare_case_test.embl -inf all"\
-        .format(os.sep, os.sep, os.sep)
+    command5 = "make_database.py -o tests{}test_data_base -n test -i tests{}rare_case_test.embl -inf all"\
+        .format(os.sep, os.sep)
     test_protein_database(command5)
     print("Finished running rare case test...")
     print()
@@ -98,9 +98,9 @@ if __name__ == "__main__":
     print("Starting Makedatabase with raw nucleotide data file:")
     # note this takes longer because of the large sequence present. This will always be the case for large
     # sequences in files.
-    command6 = "..{}make_database.py -o tests{}test_data_base -n test -i tests{}rare_case_test.embl" \
+    command6 = "make_database.py -o tests{}test_data_base -n test -i tests{}rare_case_test.embl" \
                " tests{}nucleotide_db_test.fasta tests{}GCA_000204155.1_ASM20415v1_genomic.gbk -inf all -t nucl"\
-        .format(os.sep, os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep, os.sep)
     test_nucleotide_database(command6)
     print("Finished running nucleotide database test...")
     print()
--- MultiGeneBlast/test_multigeneblast.py
+++ MultiGeneBlast/test_multigeneblast.py
@@ -22,7 +22,7 @@ def test_multigeneblast(command, outfolder, remove=True):
 
     expected_files = ['run.log', 'jquery-1.4.2.min.js', 'jquery.svg.js', 'jquery.svgdom.js', 'style.css']
     my_path = os.path.abspath(os.path.dirname(__file__))
-    db_folder = os.path.join(my_path, outfolder)
+    db_folder = os.path.join(my_path, "tests{}{}".format(os.sep, outfolder))
 
     # check if all files are present
     for path, subdirs, files in os.walk(db_folder):
@@ -45,63 +45,63 @@ def test_multigeneblast(command, outfolder, remove=True):
 if __name__ == "__main__":
     # command using gb query
     print("Starting run with genbank file:")
-    command1 = "..{}multigeneblast.py -in test_querys{}V_maris_query.gb -from 0 -to 59320 -out tests{}test_run_gbk" \
+    command1 = "multigeneblast.py -in test_querys{}V_maris_query.gb -from 0 -to 59320 -out tests{}test_run_gbk" \
                " -db test_data_base{}v_maris.dmnd -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command1, "test_run_gbk")
     print("Finished running gb query test...")
     print()
 
     # command using embl query
     print("Starting run with embl file:")
-    command2 = "..{}multigeneblast.py -in test_querys{}V_maris_query.embl -from 0 -to 59320 -out tests{}test_run_embl" \
+    command2 = "multigeneblast.py -in test_querys{}V_maris_query.embl -from 0 -to 59320 -out tests{}test_run_embl" \
                " -db test_data_base{}v_maris.dmnd -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command2, "test_run_embl")
     print("Finished running embl query test...")
     print()
 
     # command using architecture search fasta
     print("Starting run with architecture mode(fasta file):")
-    command3 = "..{}multigeneblast.py -in test_querys{}V_maris_query_partial.fasta -out tests{}test_run_fasta -db " \
+    command3 = "multigeneblast.py -in test_querys{}V_maris_query_partial.fasta -out tests{}test_run_fasta -db " \
                "test_data_base{}v_maris.dmnd -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command3, "test_run_fasta")
     print("Finished running architecture mode test...")
     print()
 
     # command using gb query and raw nucleotide database
     print("Starting run with genbank file and raw nucleotide database:")
-    command4 = "..{}multigeneblast.py -in test_querys{}V_maris_query.gb -from 0 -to 59320 -out tests{}test_run_gbk " \
+    command4 = "multigeneblast.py -in test_querys{}V_maris_query.gb -from 0 -to 59320 -out tests{}test_run_gbk " \
                "-db test_data_base{}v_maris_nuc.nal -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command4, "test_run_gbk")
     print("Finished running gb query test...")
     print()
 
     # command using embl query and raw nucleotide database
     print("Starting run with embl file and raw nucleotide database:")
-    command5 = "..{}multigeneblast.py -in test_querys{}V_maris_query.embl -from 0 -to 59320 -out tests{}test_run_embl "\
+    command5 = "multigeneblast.py -in test_querys{}V_maris_query.embl -from 0 -to 59320 -out tests{}test_run_embl "\
                "-db test_data_base{}v_maris_nuc.nal -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command5, "test_run_embl")
     print("Finished running embl query test...")
     print()
 
     # command using architecture search fasta and raw nucleotide database
     print("Starting run with architecture mode(fasta file) and raw nucleotide database:")
-    command6 = "..{}multigeneblast.py -in test_querys{}V_maris_query_partial.fasta -out tests{}test_run_fasta -db test"\
+    command6 = "multigeneblast.py -in test_querys{}V_maris_query_partial.fasta -out tests{}test_run_fasta -db test"\
                "_data_base{}v_maris_nuc.nal -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command6, "test_run_fasta")
     print("Finished running architecture mode test...")
     print()
 
     # command testing on a raw nucleotide database made from fasta embl and genbank files
     print("Starting run with raw nucleotide database made from fasta embl and genbank files:")
-    command7 = "..{}multigeneblast.py -in test_querys{}V_maris_query.gb -out tests{}test_run_fasta -from 0 -to 59320 " \
+    command7 = "multigeneblast.py -in test_querys{}V_maris_query.gb -out tests{}test_run_fasta -from 0 -to 59320 " \
                "-db test_data_base{}mix.nal -c 4 -hpg 200 -msc 25 -mpi 30 -dkb 20 -sw 0.5 -m n -op 3 -inf all"\
-        .format(os.sep, os.sep, os.sep, os.sep)
+        .format(os.sep, os.sep, os.sep)
     test_multigeneblast(command7, "test_run_fasta")
     print("Finished mixed test...")
     print()
